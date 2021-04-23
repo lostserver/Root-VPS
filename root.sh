069203500aa4d869edc1cd11df2c03c5913cfdbf
syntax-err0rr <83074099+syntax-err0rr@users.noreply.github.com>
2021-04-23 19:51:20 +0700

    Add files via upload

---
+++ root.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Simpel Change Root For Linux ( ALL LINUX OS )
+# Author = LostServer
+# ================================================
+
+# Clear
+clear
+rm -f root.sh
+
+# Get Passwd
+echo "by LostServer"
+echo ""
+read -p "Please Input an New Password For Root : " Pass
+
+# Starting Changes To Root
+rm -f /etc/ssh/sshd_config
+wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/syntax-err0rr/Root-VPS/main/sshd
+echo -e "$Pass\n$Pass\n"|passwd root &> /dev/null
+IPANDA=$(wget -qO- ifconfig.co);
+
+# Successful
+clear
+echo "Please Save This VPS Account Information"
+echo "============================================"
+echo "  IP ADDRESS = $IPANDA"
+echo "  Username   = root"
+echo "  Password   = $Pass"
+echo "============================================"
+echo ""
+echo "VPS Will Rebooting On 10 Second !!!!!"
+
+rm -f root.sh
+sleep 10
+reboot
+ 
