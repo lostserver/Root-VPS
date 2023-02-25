67d015647e4f4dcd388d04a5d015d6c75e77bebc
syntaxerorrr <83074099+lostserver@users.noreply.github.com>
2023-02-25 18:41:56 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
-# Simpel Change Root For Linux ( ALL LINUX OS )
-# Author = LostServer
-# ================================================
-
-# Clear
 clear
-rm -f root.sh
-
 # Get Passwd
-echo ""
-echo "by LostServer"
-echo ""
+echo
+echo "======================================================"
 read -p "Please Input an New Password For Root : " Pass
 
 # Starting Changes To Root
 rm -f /etc/ssh/sshd_config
-wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/syntax-err0rr/Root-VPS/main/sshd
+wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/lostserver/Root-VPS/main/sshd
 echo -e "$Pass\n$Pass\n"|passwd root &> /dev/null
 MYIP=$(curl -sS ipinfo.io/ip);
 
 # Successful
 clear
 echo "Please Save This VPS Account Information"
-echo ""
-echo "============================================"
+echo "======================================================"
 echo "  IP ADDRESS = $MYIP"
 echo "  Username   = root"
 echo "  Password   = $Pass"
-echo "============================================"
+echo "======================================================"
 echo ""
 echo "VPS Will Rebooting On 10 Second !!!!!"
 
